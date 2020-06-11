de934dc628e0e8cb70687300c3e2a341cfa7be60
santhosh <santhosh.kumar@thoughtworks.com>
2020-06-11 13:07:20 +0530

    modified bfs function and extracted  enqueue_chidren function out of it

--- src/graph.js
+++ src/graph.js
@@ -4,26 +4,27 @@
 // To => to
 // Should return true.
 
-const bfs = function (pairs, source, target) {
-  let queue = [];
-  let visitedNodes = [];
-
+const enqueue_chidren = (pairs, parent, visitedNodes, queue) => {
   pairs.forEach((pair) => {
-    if (pair[0] == source && !visitedNodes.includes(pair[1])) {
+    if (pair[0] == parent && !visitedNodes.includes(pair[1])) {
       queue.push(pair[1]);
     }
   });
 
+  return queue;
+};
+
+const bfs = function (pairs, source, target) {
+  let queue = [];
+  let visitedNodes = [];
+  queue = enqueue_chidren(pairs, source, visitedNodes, queue);
+
   while (queue.length != 0) {
     const node = queue.shift();
     visitedNodes.push(node);
-    if (node == target) return true;
 
-    pairs.forEach((pair) => {
-      if (pair[0] == node && !visitedNodes.includes(pair[1])) {
-        queue.push(pair[1]);
-      }
-    });
+    if (node == target) return true;
+    queue = enqueue_chidren(pairs, node, visitedNodes, queue);
   }
   return false;
 };
